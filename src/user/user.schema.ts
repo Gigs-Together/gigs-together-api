811c8ee0e6c33665d06be1be26d3ce329b870894
Arina M <thearinamarr@gmail.com>
2024-11-14 14:35:03 +0100

    Fix user schema

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,17 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-// import { HydratedDocument } from 'mongoose';
+import { HydratedDocument } from 'mongoose';
 
 @Schema()
 export class User {
-  @Prop({ unique: true })
-  username: string;
-
-  @Prop({ unique: true })
-  telegramId: string;
-
   @Prop()
-  isAdmin: string;
+  username?: string;
+
+  @Prop({ unique: true })
+  telegramId: number;
+
+  @Prop({ default: false })
+  isAdmin: boolean;
 }
 
-// export type UserDocument = HydratedDocument<User>;
+export type UserDocument = HydratedDocument<User>;
 export const UserSchema = SchemaFactory.createForClass(User);
