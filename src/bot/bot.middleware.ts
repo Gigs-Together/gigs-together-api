56537907ad27e314b1c5a86bf30a3383e725c2bc
Arina M <thearinamarr@gmail.com>
2024-11-24 21:45:51 +0100

    Add bot middleware to check the secret header

---
+++ src/bot/bot.middleware.ts
@@ -0,0 +1,17 @@
+import { Injectable, NestMiddleware, ForbiddenException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class BotMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction): void {
+    const secretHeader = req.headers[
+      'x-telegram-bot-api-secret-token'
+    ] as string;
+
+    if (secretHeader !== process.env.BOT_SECRET) {
+      throw new ForbiddenException('Invalid secret token');
+    }
+
+    next();
+  }
+}
--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { BotController } from './bot.controller';
 import { BotService } from './bot.service';
 import { UserModule } from '../user/user.module';
+import { BotMiddleware } from './bot.middleware';
 
 @Module({
   imports: [UserModule],
   controllers: [BotController],
   providers: [BotService],
 })
-export class BotModule {}
+export class BotModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(BotMiddleware).forRoutes(BotController);
+  }
+}
