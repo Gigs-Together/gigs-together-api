b91dd758721177e75311e6cfb8d855db82279da8
Arina M <thearinamarr@gmail.com>
2024-12-03 14:01:16 +0100

    Simplify auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,32 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { Admin, AdminDocument } from '../schemas/admin.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class AuthService {
-  private adminsCache: Promise<AdminDocument[]>;
+  private adminsCache: AdminDocument[];
 
   constructor(
     @InjectModel(Admin.name) private adminModel: Model<AdminDocument>,
-  ) {
-    this.refreshAdminsCache();
-  }
+  ) {}
 
-  private async refreshAdminsCache(): Promise<void> {
-    this.adminsCache = this.adminModel.find({ isActive: true }).exec();
-    const admins = await this.adminsCache;
+  private async pullAdmins(): Promise<void> {
+    const admins = await this.adminModel.find({ isActive: true }).exec();
+    this.adminsCache = admins;
     console.log(`Admins cache refreshed: ${admins.length} admin(s) found.`);
   }
 
-  @Cron(CronExpression.EVERY_10_MINUTES)
-  async handleAdminsCacheSync(): Promise<void> {
-    console.log('Refreshing admins cache from the database...');
-    await this.refreshAdminsCache();
-  }
-
   async isAdmin(telegramId: number): Promise<boolean> {
-    const admins = await this.adminsCache;
-    return admins.some((admin) => admin.telegramId === telegramId);
+    if (!this.adminsCache) {
+      await this.pullAdmins();
+    }
+    return this.adminsCache.some((admin) => admin.telegramId === telegramId);
   }
 }
