53c5f82d1af57a309aa9c9683e06d4842c525c87
Arina M <thearinamarr@gmail.com>
2024-12-06 18:21:15 +0100

    Add closing an app in e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { Connection } from 'mongoose';
+import { getConnectionToken } from '@nestjs/mongoose';
+
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let connection: Connection;
+
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,6 +18,15 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    connection = app.get<Connection>(getConnectionToken());
+    connection.on('disconnected', () => {
+      console.log('Mongoose connection closed');
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
